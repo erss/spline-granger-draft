3136681df6b753d6c8ee4f73b6a0ef547fb77b4a
ELIZABETH SPENCER <erss@bu.edu>
2017-02-01 10:42:18 -0500

    Added optional parameter indicating whether or not you can plot

--- Documents/MATLAB/ar_model/mySpec.m
+++ Documents/MATLAB/ar_model/mySpec.m
@@ -1,6 +1,10 @@
-function [faxis, Sxx] = mySpec( x, f0 )
+function [faxis, Sxx] = mySpec( x, f0, plot_flag )
 % MYSPEC computes the spectrum of a signal, x, at a sampling frequency, f0.
-%  
+%  Plots if PLOT_FLAG = 1, the default
+if nargin == 2
+    plot_flag = 1 ;  % Default value 
+end
+
 
 dt = 1/f0;
 T = length(x)/f0;  % total length of recording (seconds)
@@ -15,11 +19,14 @@ Sxx = Sxx(1:length(x)/2+1);   % Remove negative frequencies
 
 faxis = 0:df:fNQ;             % Frequency axis
 
-plot(faxis,Sxx);     
-xlim([0 f0/4]);
-xlabel('Frequency (Hz)','FontSize',15);
-ylabel('Power','FontSize',15);
-title('Spectrogram','FontSize',15);
+
+    if plot_flag == 1
+        plot(faxis,Sxx);     
+        xlim([0 f0/4]);
+        xlabel('Frequency (Hz)','FontSize',15);
+        ylabel('Power','FontSize',15);
+        title('Spectrogram','FontSize',15);
+    end
 
 
 end
