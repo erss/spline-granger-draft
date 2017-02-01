0b18f60d63339d5107d69588220c79e7f53057f5
ELIZABETH SPENCER <erss@bu.edu>
2017-02-01 10:41:30 -0500

    Made more efficient

--- Documents/MATLAB/ar_model/myPrediction.m
+++ Documents/MATLAB/ar_model/myPrediction.m
@@ -1,11 +1,11 @@
-function [ y] = myPrediction( data, modelCoef, nlags)
+function [ y] = myPrediction( data, model_coef, nlags)
 % MYPREDICTION computes the one-step prediction for an AR network model of
 % coefficients
 %
 % INPUTS:
 %  data           = A matrix of electode data with dimensions electrodes x
 %                    time
-%  modelCoef      = model coefficients with dimensions electrodes x number 
+%  model_coef      = model coefficients with dimensions electrodes x number 
 %                    predictor variables
 %  nlags          = The number of lags used as used for each signal
 % 
@@ -16,20 +16,15 @@ function [ y] = myPrediction( data, modelCoef, nlags)
 nelectrodes = size(data,1);
 
 %%% Build predictor variables--------------------------------------------
-   X= [];
-for k = 1:nelectrodes
-    X_temp = []; 
-    sgnl = data(k,:)';
-    for i=1:nlags                                   %For each lag,
-        X_temp = [X_temp, circshift(sgnl,i)];   %... shift x and store it.
-     
-    end
-    X_temp = X_temp(1,:); % use most recent data 
-    X = [X X_temp];
+   
+X= [];
+   
+for n = 1:nelectrodes
+    X = [X, data(n,end:-1:end-nlags+1)];
 end
 
 %%% Put into model--------------------------------------------------------
-  y = X*modelCoef';
+  y = X*model_coef';
   y = y';
 
 end
