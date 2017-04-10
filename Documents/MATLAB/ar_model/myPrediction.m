4d9a0864d9dc43563d20a7fdfc9177b0cd3075ee
ELIZABETH SPENCER <erss@bu.edu>
2017-04-10 15:59:01 -0400

    edited documentation

--- Documents/MATLAB/ar_model/myPrediction.m
+++ Documents/MATLAB/ar_model/myPrediction.m
@@ -5,16 +5,26 @@ function [ y] = myPrediction( data, model_coefficients)
 % INPUTS:
 %  data           = A matrix of electode data with dimensions electrodes x
 %                    time
-%  model_coef      = model coefficients with dimensions electrodes x number 
-%                    predictor variables
-% 
+%  model_coef      = model coefficients with dimensions electrodes x
+%                              electrodes x number time lags, i.e. if
+%                              model_coeff is 3 x 3 x 10, then there are 3
+%                              electrodes in the network whose activity is
+%                              deterined by 10 time steps in the past.  The
+%                              influence at lag = 1 is given by 
+%                              model_coeff(:,:,1). This gives the network
+%                              connectivity at a lag 1, where the rows
+%                              represent each electrode fit, i. e.
+%                              model_coeff(1,3,1) is the influence of
+%                              electrode 3 on electrode 1 at one time lag
+%                              in the past
+%                             
 % OUTPUTS:
 %  y =  The one-step prediction for each electrode in network with
 %       dimensions [number of electrodes] x [1].
 %
 % Example for simulating a signal:
 % for k = nlags:length(data)-1;
-%     data(:,k+1) = myPrediction(data(:,1:k),model_coef,nlags);
+%     data(:,k+1) = myPrediction(data(:,1:k),model_coef);
 %     data(:,k+1) = data(:,k+1) + noise.*randn(size(data,1),1);
 % end
 %
