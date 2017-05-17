de81f550fc77ace850fa90293a3d23cbe5bec19e
ELIZABETH SPENCER <erss@bu.edu>
2017-05-17 11:43:50 -0400

    Fixed bug if network fit shows no dependence on any electrode

--- Network_building/estimate_coefficient_fits.m
+++ Network_building/estimate_coefficient_fits.m
@@ -1,4 +1,4 @@
-function [bhat, yhat] = estimate_coefficient_fits( data, adj_mat, model_order, c_pt_times )
+function [bhat, yhat] = estimate_coefficient_fits( data, adj_mat, model_order, cntrl_pts )
 % ESTIMATE_COEFFICIENT_FITS builds network model from MVAR spline modeling 
 %
 % INPUTS:
@@ -28,7 +28,7 @@ function [bhat, yhat] = estimate_coefficient_fits( data, adj_mat, model_order, c
 s = 0.5;                                    % Define Tension Parameter
 
 % Construct spline regressors.
-c_pt_times_all = [c_pt_times(1)-2 c_pt_times c_pt_times(end)+2];
+c_pt_times_all = [cntrl_pts(1)-2 cntrl_pts cntrl_pts(end)+2];
 Z = zeros(model_order,length(c_pt_times_all));
 num_c_pts = length(c_pt_times_all);  %number of control points in total
 for i=1:model_order
@@ -43,49 +43,52 @@ end
 
 
 for electrode = 1:nelectrodes
+    
+   
     model_size = sum(adj_mat(electrode,:));
-    Z0 = kron(eye(model_size),Z);   % Nested model spline regressors
-    
-    % Generate observations for given y
-    x = data(electrode,:);
-    y = x(model_order+1:end);   
-    y = y';
-    
-    % Build history matrix
-     X = []; 
-    % Generate regressors for network structure
-     indices = logical(adj_mat(electrode,:));
-     data_subset = data(indices,:);
-     
-    for k = 1:model_size
-        X_temp = []; 
-        sgnl = data_subset(k,:)';
-        
+    if model_size > 0
+        Z0 = kron(eye(model_size),Z);   % Nested model spline regressors
 
-        for i=1:model_order                                   %For each lag,
-            X_temp = [X_temp, circshift(sgnl,i)];   %... shift x and store it.
+        % Generate observations for given y
+        x = data(electrode,:);
+        y = x(model_order+1:end);   
+        y = y';
+
+        % Build history matrix
+         X = []; 
+        % Generate regressors for network structure
+         indices = logical(adj_mat(electrode,:));
+         data_subset = data(indices,:);
+
+        for k = 1:model_size
+            X_temp = []; 
+            sgnl = data_subset(k,:)';
+
+
+            for i=1:model_order                                   %For each lag,
+                X_temp = [X_temp, circshift(sgnl,i)];   %... shift x and store it.
+            end
+            X_temp = X_temp(model_order+1:end,:);  
+
+
+        X = [X X_temp];
+
         end
-        X_temp = X_temp(model_order+1:end,:);  
+       % 
+        Xfull = X * Z0;  % regressors for y_hat = X0*Z0*a0
+        [alpha,~,~] = glmfit(Xfull,y,'normal','constant','off');
 
+        yhat(electrode,:) = glmval(alpha,Xfull,'identity','constant','off'); % Get signal estimate.
+        b = Z0*alpha;  
 
-    X = [X X_temp];
-                
+        j=1;
+          for p = 1:nelectrodes
+            if adj_mat(electrode,p) == 1
+                bhat(electrode,p,:) = b(j:j+model_order-1);
+                j= j+model_order;
+            end    
+         end
     end
-   % 
-    Xfull = X * Z0;  % regressors for y_hat = X0*Z0*a0
-    [alpha,~,~] = glmfit(Xfull,y,'normal','constant','off');
-    
-    yhat(electrode,:) = glmval(alpha,Xfull,'identity','constant','off'); % Get signal estimate.
-    b = Z0*alpha;  
-    
-    j=1;
-      for p = 1:nelectrodes
-        if adj_mat(electrode,p) == 1
-            bhat(electrode,p,:) = b(j:j+model_order-1);
-            j= j+model_order;
-        end    
-     end
-    
 
 end
 
