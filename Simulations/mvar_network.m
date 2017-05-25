8213f27a1af339fe345d4cb4e95efec8bd5b98d7
ELIZABETH SPENCER <erss@bu.edu>
2017-05-25 12:44:34 -0400

    Made simulation with MVAR toolbox to see how our toolbox compares

---
+++ Simulations/mvar_network.m
@@ -0,0 +1,100 @@
+ntrials   = 1;     % number of trials
+nobs      = 1000;   % number of observations per trial
+
+regmode   = 'OLS';  % VAR model estimation regression mode ('OLS', 'LWR' or empty for default)
+icregmode = 'LWR';  % information criteria regression mode ('OLS', 'LWR' or empty for default)
+
+morder    = 'AIC';  % model order to use ('actual', 'AIC', 'BIC' or supplied numerical value)
+momax     = 20;     % maximum model order for model order estimation
+
+acmaxlags = 1000;   % maximum autocovariance lags (empty for automatic calculation)
+
+tstat     = '';     % statistical test for MVGC:  'F' for Granger's F-test (default) or 'chi2' for Geweke's chi2 test
+alpha     = 0.05;   % significance level for significance test
+mhtc      = 'FDR';  % multiple hypothesis test correction (see routine 'significance')
+
+fs        = 200;    % sample rate (Hz)
+fres      = [];     % frequency resolution (empty for automatic calculation)
+
+seed      = 0;      % random seed (0 for unseeded)
+
+% Generate VAR test data (<mvgc_schema.html#3 |A3|>)
+%
+% _*Note:*_ This is where you would read in your own time series data; it should
+% be assigned to the variable |X| (see below and <mvgchelp.html#4 Common
+% variable names and data structures>).
+
+% Seed random number generator.
+
+rng_seed(seed);
+
+
+% Get VAR coefficients for 5-node test network.
+
+AT = var9_test; %var5_test;
+nvars = size(AT,1); % number of variables
+
+% Residuals covariance matrix.
+
+SIGT = eye(nvars);
+
+% Generate multi-trial VAR time series data with normally distributed residuals
+% for specified coefficients and covariance matrix.
+
+ptic('\n*** var_to_tsdata... ');
+X = var_to_tsdata(AT,SIGT,nobs,ntrials);
+ptoc;
+
+%
+data = X;
+model_order = 40;
+global s;
+s = 0.5;
+global nsurrogates;
+nsurrogates = 100;
+f0=fs;
+noise=1;
+N=size(data,2);
+T= N/f0;
+dt=1/f0;
+nlags = 3;
+nelectrodes = size(data,1);
+%%% Fit spline to data ---------------------------------------------------
+
+cntrl_pts = make_knots(model_order,10);
+tic
+[ adj_mat] = build_ar_splines( data, model_order, cntrl_pts );
+splinetime  = toc;
+[ bhat, yhat ] = estimate_coefficient_fits( data, adj_mat, model_order, cntrl_pts);
+
+
+%%% Plot results ----------------------------------------------------------
+adj_true = sum(AT,3);
+adj_true(adj_true~=0) = 1;
+
+subplot(2,3,4)
+imagesc(sum(AT,3))
+title('True Network')
+
+subplot(2,3,6)
+plotNetwork(adj_mat)
+title('Spline Network')
+title(strcat({'Spline, '},num2str(splinetime),{' s'}))
+
+Sampling_Frequency = f0;
+Noise_Variance = noise.^2;
+T_seconds = T;
+Model_Order = nlags;
+Estimated_Order = model_order;
+Tension_Parameter = s;
+Tp = table(Sampling_Frequency,Noise_Variance,T_seconds,Model_Order,Estimated_Order,Tension_Parameter);
+figure;
+uitable('Data',Tp{:,:},'ColumnName',Tp.Properties.VariableNames,...
+    'RowName',Tp.Properties.RowNames,'Units', 'Normalized', 'Position',[0, 0, 1, 1]);
+
+b=AT;
+%goodness_of_fit_spectrum;
+goodness_of_fit_bootstrap;
+
+
+
