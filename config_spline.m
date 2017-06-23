7e225f85c08993dcbb540d82d964c49f9af49003
ELIZABETH SPENCER <erss@bu.edu>
2017-06-23 11:32:08 -0400

    added comments

--- config_spline.m
+++ config_spline.m
@@ -1,7 +1,7 @@
-%%% Model type
+%%% Model type ------------------------------------------------------------
 model_true.noise_type = 'white'; % 'white', 'real'
 
-%%% Simulation parameters
+%%% Simulation parameters -------------------------------------------------
 
 model_true.sampling_frequency = 500;
 model_true.T = 2;   % time in seconds of window
@@ -11,7 +11,7 @@ if strcmp(model_true.noise_type,'white')
     model_true.true_coefficients = three_node_sim_1; %%%% MODIFY COEFFICIENTS HERE!
     model_true.model_coefficients = model_true.true_coefficients;
 end
-%%% Define model inputs for spline Granger & standard Granger
+%%% Define model inputs for spline Granger & standard Granger -------------
 
 model_true.s = 0.5;                     % tension parameter for spline
 model_true.estimated_model_order = 50;  % model_order used to estimate
@@ -19,12 +19,10 @@ model_true.estimated_model_order = 50;  % model_order used to estimate
 number_of_knots       = floor(model_true.estimated_model_order/3);
 model_true.cntrl_pts = make_knots(model_true.estimated_model_order,number_of_knots);
 
-%%% Define network testing parameters
+%%% Define network testing parameters -------------------------------------
 
-model_true.q = 0.05; % FDR max number acceptable proportion of false discoveries
-
-%%% Define inputs for model testing
-model_true.nsurrogates = 100; % number of surrogates used for bootstrapping
+model_true.q = 0.05;            % FDR max number acceptable proportion of false discoveries
+model_true.nsurrogates = 100;   % number of surrogates used for bootstrapping
 model_true.nrealizations = 100; % number of realizations used for spectral testing
 
 
