846bef59604cb2ca0cc8ca28ba8053ba08fa6756
Mark-Kramer <mak@math.bu.edu>
2017-06-30 14:03:10 -0400

    Updated main_spline call.

--- main_spline.m
+++ main_spline.m
@@ -1,15 +1,15 @@
-clear all; close all;
+clear; close all;
 clc;
-%% Configure parameters
+%%% Configure parameters
 config_spline;
 
-%% Simulate data
+%%% Simulate data
 simulate_network;
 
-%% Fit Networks
+%%% Fit Networks
 infer_network;
 
-%% Plot stuff
+%%%% Plot stuff
 plot_data;
 
 % figure; plotchannels(model_true.data');
@@ -20,3 +20,17 @@ plot_data;
 % gof_bootstrap(model_true,model_spline,model_standard);
 % gof_residuals(model_spline);
 % gof_spectrum(model_true,model_spline,model_standard);
+
+%%% Goodness of Fit & Plots
+gof_bootstrap(model_true,model_spline,model_standard);
+gof_residuals(model_spline);
+gof_spectrum(model_true,model_spline,model_standard);
+
+%%% Compute g.o.f with Bartlett test.
+Ij_low = [0,4; 4,8;  8,12; 12,20; 20,30; 30,50];
+[result] = gof_bartlett(model_spline, Ij_low);
+      plot_gof_bartlett(result,       Ij_low);
+
+Ij_high = (50:5:120)';  Ij_high = [Ij_high(1:end-1), Ij_high(2:end)];
+[result] = gof_bartlett(model_spline, Ij_high);
+      plot_gof_bartlett(result,       Ij_high);
