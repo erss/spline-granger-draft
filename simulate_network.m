1bbde09a40bb00f94771061b78b82c1bf1b8681d
ELIZABETH SPENCER <erss@bu.edu>
2017-08-21 14:36:43 -0400

    filtered real data at 60hz

--- simulate_network.m
+++ simulate_network.m
@@ -19,6 +19,12 @@ elseif strcmp(model_true.noise_type,'real')   %%%%% if real data edit this
     data_sz= data(ntwk,sz);
     data_presz= data(ntwk,pre_sz);
     
+    d = designfilt('bandstopiir','FilterOrder',2, ...
+               'HalfPowerFrequency1',59,'HalfPowerFrequency2',61, ...
+               'DesignMethod','butter','SampleRate',500);
+           
+    data_sz = filtfilt(d,data_sz')';
+    data_presz = filtfilt(d,data_presz')';
     if strcmp(model_true.sztype,'sz')
         model_true.data = data_sz;
     elseif strcmp(model_true.sztype,'presz')
